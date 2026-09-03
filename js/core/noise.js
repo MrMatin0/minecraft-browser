c0986f6ee439723925e4ab65b827f4db24786c0f
MrMatin0 <86603224+MrMatin0@users.noreply.github.com>
2026-09-03 19:38:09 +0330

    perf(core,gfx): flatten noise gradients, cache input rect, kill per-frame allocations in sky
    - noise: flatten grad3 array-of-arrays into a typed Int8Array (removes 4 nested
      array lookups per noise sample; worldgen calls this millions of times)
    - input: stop calling getBoundingClientRect() on every mousemove (layout thrash);
      cache it and invalidate on resize/scroll. Expose pressedCodes so callers can
      iterate only the keys pressed this frame instead of Object.keys(allKeysEver)
    - shaders: discard before sampling the overlay texture (saves a texture fetch on
      every discarded fragment) and hoist the light curve
    - sky: Sky.update() allocated ~10 THREE.Color/Vector3 objects *per frame*. Now
      reuses module-scope scratch objects. Also removed dead code (unused dayLight
      locals, redundant per-frame sun/moon position + rotation resets)

--- js/core/noise.js
+++ js/core/noise.js
@@ -2,7 +2,14 @@
 MC.Shared.push(function defineNoise(ns) {
   var F2 = 0.5 * (Math.sqrt(3) - 1), G2 = (3 - Math.sqrt(3)) / 6;
   var F3 = 1 / 3, G3 = 1 / 6;
-  var grad3 = [[1, 1, 0], [-1, 1, 0], [1, -1, 0], [-1, -1, 0], [1, 0, 1], [-1, 0, 1], [1, 0, -1], [-1, 0, -1], [0, 1, 1], [0, -1, 1], [0, 1, -1], [0, -1, -1]];
+  // Gradient table flattened into a typed array: GRAD[gi * 3 + axis].
+  // The old array-of-arrays version cost a pointer chase per component and this is
+  // the single hottest inner loop in world generation.
+  var GRAD = new Int8Array([
+    1, 1, 0, -1, 1, 0, 1, -1, 0, -1, -1, 0,
+    1, 0, 1, -1, 0, 1, 1, 0, -1, -1, 0, -1,
+    0, 1, 1, 0, -1, 1, 0, 1, -1, 0, -1, -1
+  ]);
 
   function Noise(seed) {
     var s = (seed | 0) || 1;
@@ -14,24 +21,24 @@ MC.Shared.push(function defineNoise(ns) {
     for (i = 0; i < 512; i++) { this.perm[i] = p[i & 255]; this.permMod12[i] = this.perm[i] % 12; }
   }
   Noise.prototype.noise2D = function (xin, yin) {
-    var perm = this.perm, permMod12 = this.permMod12;
-    var n0, n1, n2;
+    var perm = this.perm, permMod12 = this.permMod12, g = GRAD;
+    var n0 = 0, n1 = 0, n2 = 0;
     var s = (xin + yin) * F2; var i = Math.floor(xin + s), j = Math.floor(yin + s);
-    var t = (i + j) * G2; var X0 = i - t, Y0 = j - t; var x0 = xin - X0, y0 = yin - Y0;
+    var t = (i + j) * G2; var x0 = xin - (i - t), y0 = yin - (j - t);
     var i1, j1; if (x0 > y0) { i1 = 1; j1 = 0; } else { i1 = 0; j1 = 1; }
     var x1 = x0 - i1 + G2, y1 = y0 - j1 + G2, x2 = x0 - 1 + 2 * G2, y2 = y0 - 1 + 2 * G2;
     var ii = i & 255, jj = j & 255;
     var t0 = 0.5 - x0 * x0 - y0 * y0;
-    if (t0 < 0) n0 = 0; else { var gi0 = permMod12[ii + perm[jj]]; t0 *= t0; n0 = t0 * t0 * (grad3[gi0][0] * x0 + grad3[gi0][1] * y0); }
+    if (t0 > 0) { var k0 = permMod12[ii + perm[jj]] * 3; t0 *= t0; n0 = t0 * t0 * (g[k0] * x0 + g[k0 + 1] * y0); }
     var t1 = 0.5 - x1 * x1 - y1 * y1;
-    if (t1 < 0) n1 = 0; else { var gi1 = permMod12[ii + i1 + perm[jj + j1]]; t1 *= t1; n1 = t1 * t1 * (grad3[gi1][0] * x1 + grad3[gi1][1] * y1); }
+    if (t1 > 0) { var k1 = permMod12[ii + i1 + perm[jj + j1]] * 3; t1 *= t1; n1 = t1 * t1 * (g[k1] * x1 + g[k1 + 1] * y1); }
     var t2 = 0.5 - x2 * x2 - y2 * y2;
-    if (t2 < 0) n2 = 0; else { var gi2 = permMod12[ii + 1 + perm[jj + 1]]; t2 *= t2; n2 = t2 * t2 * (grad3[gi2][0] * x2 + grad3[gi2][1] * y2); }
+    if (t2 > 0) { var k2 = permMod12[ii + 1 + perm[jj + 1]] * 3; t2 *= t2; n2 = t2 * t2 * (g[k2] * x2 + g[k2 + 1] * y2); }
     return 70 * (n0 + n1 + n2);
   };
   Noise.prototype.noise3D = function (xin, yin, zin) {
-    var perm = this.perm, permMod12 = this.permMod12;
-    var n0, n1, n2, n3;
+    var perm = this.perm, permMod12 = this.permMod12, g = GRAD;
+    var n0 = 0, n1 = 0, n2 = 0, n3 = 0;
     var s = (xin + yin + zin) * F3; var i = Math.floor(xin + s), j = Math.floor(yin + s), k = Math.floor(zin + s);
     var t = (i + j + k) * G3; var x0 = xin - (i - t), y0 = yin - (j - t), z0 = zin - (k - t);
     var i1, j1, k1, i2, j2, k2;
@@ -42,13 +49,13 @@ MC.Shared.push(function defineNoise(ns) {
     var x3 = x0 - 1 + 3 * G3, y3 = y0 - 1 + 3 * G3, z3 = z0 - 1 + 3 * G3;
     var ii = i & 255, jj = j & 255, kk = k & 255;
     var t0 = 0.6 - x0 * x0 - y0 * y0 - z0 * z0;
-    if (t0 < 0) n0 = 0; else { var g0 = grad3[permMod12[ii + perm[jj + perm[kk]]]]; t0 *= t0; n0 = t0 * t0 * (g0[0] * x0 + g0[1] * y0 + g0[2] * z0); }
+    if (t0 > 0) { var m0 = permMod12[ii + perm[jj + perm[kk]]] * 3; t0 *= t0; n0 = t0 * t0 * (g[m0] * x0 + g[m0 + 1] * y0 + g[m0 + 2] * z0); }
     var t1 = 0.6 - x1 * x1 - y1 * y1 - z1 * z1;
-    if (t1 < 0) n1 = 0; else { var g1 = grad3[permMod12[ii + i1 + perm[jj + j1 + perm[kk + k1]]]]; t1 *= t1; n1 = t1 * t1 * (g1[0] * x1 + g1[1] * y1 + g1[2] * z1); }
+    if (t1 > 0) { var m1 = permMod12[ii + i1 + perm[jj + j1 + perm[kk + k1]]] * 3; t1 *= t1; n1 = t1 * t1 * (g[m1] * x1 + g[m1 + 1] * y1 + g[m1 + 2] * z1); }
     var t2 = 0.6 - x2 * x2 - y2 * y2 - z2 * z2;
-    if (t2 < 0) n2 = 0; else { var g2 = grad3[permMod12[ii + i2 + perm[jj + j2 + perm[kk + k2]]]]; t2 *= t2; n2 = t2 * t2 * (g2[0] * x2 + g2[1] * y2 + g2[2] * z2); }
+    if (t2 > 0) { var m2 = permMod12[ii + i2 + perm[jj + j2 + perm[kk + k2]]] * 3; t2 *= t2; n2 = t2 * t2 * (g[m2] * x2 + g[m2 + 1] * y2 + g[m2 + 2] * z2); }
     var t3 = 0.6 - x3 * x3 - y3 * y3 - z3 * z3;
-    if (t3 < 0) n3 = 0; else { var g3 = grad3[permMod12[ii + 1 + perm[jj + 1 + perm[kk + 1]]]]; t3 *= t3; n3 = t3 * t3 * (g3[0] * x3 + g3[1] * y3 + g3[2] * z3); }
+    if (t3 > 0) { var m3 = permMod12[ii + 1 + perm[jj + 1 + perm[kk + 1]]] * 3; t3 *= t3; n3 = t3 * t3 * (g[m3] * x3 + g[m3 + 1] * y3 + g[m3 + 2] * z3); }
     return 32 * (n0 + n1 + n2 + n3);
   };
   // fractal brownian motion helpers
